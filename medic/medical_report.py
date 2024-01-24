83a4dd7c69cb9f26ef73f84743d8c0c36c4cfea0
cushog74 <145211800+cushog74@users.noreply.github.com>
2024-01-24 21:45:59 +0300

    2

--- medic/medical_report.py
+++ medic/medical_report.py
@@ -2,18 +2,18 @@ import pandas as pd
 
 def find_patient_by_surname_and_date(surname, date):
     data = pd.read_csv("patient_data.csv")
-    filtered_data = data[(data["Фамилия"] == surname) & (data["Дата об-ращения"] == date)]
+    filtered_data = data[(data["Фамилия"] == surname) & (data["Дата обращения"] == date)]
     return filtered_data
 
 def build_yearly_disease_table(data):
-    data["Дата об-ращения"] = pd.to_datetime(data["Дата об-ращения"]).dt.year
-    yearly_disease_counts = data.groupby("Дата об-ращения").size()
+    data["Дата обращения"] = pd.to_datetime(data["Дата обращения"]).dt.year
+    yearly_disease_counts = data.groupby("Дата обращения").size()
     yearly_disease_counts.index = yearly_disease_counts.index.astype(str)
     return yearly_disease_counts
 
 def main():
     surname = input("Введите фамилию пациента: ")
-    date = input("Введите дату об-ращения: ")
+    date = input("Введите дату обращения: ")
     patient_record = find_patient_by_surname_and_date(surname, date)
 
     if not patient_record.empty:
@@ -21,7 +21,7 @@ def main():
         print("Число заболевших по дате рождения:")
         print(yearly_disease_counts)
     else:
-        print("Записи с таким именем и датой об-ращения не найдены.")
+        print("Записи с таким именем и датой обращения не найдены.")
 
 if __name__ == "__main__":
     main()
